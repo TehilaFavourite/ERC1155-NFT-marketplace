99b01c85a74b71d6dda0655f3f56c6ff236c0fff
tehilafavourite <tehilafavourite@gmail.com>
2022-10-20 14:38:38 +0100

    updated buy function and listToken function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -23,10 +23,10 @@ contract  Marketplace is ERC1155Holder {
 
 struct ListedToken {
     uint256 tokenId;
-    uint256 nftId;
+    // uint256 nftId;
     uint256 amount;
     uint256 price;
-    uint256 royalty;
+    // uint256 royalty;
     address payable seller;
     address payable buyer;
     address payable owner;
@@ -49,16 +49,18 @@ modifier onlyOwner() {
     _;
 }
 
-function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed, bool forSale) external {
+function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed, bool forSale, bool _sold) external {
     ListedToken storage _listedToken = listedToken[_tokenid];
     _listedToken.seller = payable(msg.sender);
-    require(nftContract.balanceOf(msg.sender, _tokenid) != 0, "you do not own this NFT"); 
+    require(nftContract.balanceOf(msg.sender, _tokenid) != 0, "you do not own this NFT");
+    require(!_listedToken.sold, "This token Id has been sold"); 
     // require(_listedToken.tokenId > 0, "Token does not exist");
     // require(_listedToken.royalty > 0 && _listedToken.royalty < 30, "royalty should be less than 30");
     // require(nftContract.ownerOf(_tokenid) == msg.sender, "You do not own an NFT");
 
     _listedToken.tokenId =  _tokenid;
     _listedToken.isForSale = forSale;
+    _listedToken.sold = _sold;
     _listedToken.amount = _amount;
     _listedToken.price = _price;
     // _listedToken.royalty = _royalty;
@@ -67,35 +69,51 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
     IERC1155(nftContract).safeTransferFrom(msg.sender, address(this), _tokenid, _amount, "");
     }
 
+    function userNFTBal(address _addr, uint256 _tokenid) external view returns (uint256) {
+        return nftContract.balanceOf(_addr, _tokenid);
+    }
+
     function calBuyerfee(uint256 fee) internal pure returns(uint256) {
-        return fee * 5 / 100;
+        return fee * 10 / 100;
     }
 
-    function calSellerfee(uint256 fee) internal pure returns(uint256) {
-        return fee * 5 / 100;
-    }
+    // function calSellerfee(uint256 fee) internal pure returns(uint256) {
+    //     return fee * 5 / 100;
+    // }
 
-    function calRoyaltyfee(uint256 fee) internal pure returns(uint256) {
-        return fee * 3 / 100;
-    }
+    // function calRoyaltyfee(uint256 fee) internal pure returns(uint256) {
+    //     return fee * 3 / 100;
+    // }
 
     function buyNft(uint256 _tokenid, uint256 _amount) external payable {
         ListedToken storage _listedToken = listedToken[_tokenid];
 
         uint256 price = listedToken[_tokenid].price;
         _listedToken.buyer = payable(msg.sender);
-        require(listedToken[_tokenid].isForSale == true, "property is not for sale");
+        require(listedToken[_tokenid].isForSale, "Id is not for sale");
         require(msg.value >= price, "Price is not complete");
+        require(listedToken[_tokenid].currentlyListed, "Id is not for currently listed");
 
+        uint256 buyerFee = calBuyerfee(listedToken[_tokenid].price);
+        // uint256 royaltyFee = calRoyaltyfee(listedToken[_tokenid].royalty);
+        // uint256 totalFeePaid = buyerFee + royaltyFee;
+        // uint256 finalPrice = price - totalFeePaid;
+        uint256 finalFee = price - buyerFee;
 
-        uint256 buyerFee = calBuyerfee(fees[_tokenid].buyerFee) * price;
-        uint256 royaltyFee = calRoyaltyfee(listedToken[_tokenid].royalty) * price;
-        uint256 totalFeePaid = buyerFee + royaltyFee;
-        uint256 finalPrice = price - totalFeePaid;
-
-        payable(listedToken[_tokenid].seller).transfer(finalPrice);
-        payable(listedToken[_tokenid].owner).transfer(buyerFee);
+        payable(listedToken[_tokenid].seller).transfer(finalFee);
+        // payable(listedToken[_tokenid].seller).transfer(royaltyFee);
+        payable(address(this)).transfer(buyerFee);
         IERC1155(nftContract).safeTransferFrom(address(this), msg.sender, _tokenid, _amount, "");
     }
 
+    function getMarketplaceBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getUserBalance(address _user) external view returns (uint256) {
+        return address(_user).balance;
+    }
+
 }
+
+
