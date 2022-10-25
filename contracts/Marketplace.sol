9604beccd2bd441e214917c7923af397081469a4
tehilafavourite <tehilafavourite@gmail.com>
2022-10-25 19:19:31 +0100

    updated cancelList function

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -13,8 +13,28 @@ contract  Marketplace is ERC1155Holder {
     Counters.Counter private _nftSold;
     IERC1155 private nftContract;
     address private owner;
-    // uint256 private platformFee = 25;
-    // uint256 private deno = 1000;
+
+event ListedProperty
+    (
+        address indexed _seller, 
+        uint256 _tokenId, 
+        uint256 _price,
+        uint256 _amount, 
+        uint256 _time, 
+        bool _forSale,
+        bool _currentlyListed,
+        bool sold
+        );
+
+event BuyEvent 
+    (
+    address _buyer,
+    address _token,
+    uint256 _tokenId,
+    uint256 _amountOfToken,
+    uint256 _price
+  );
+
 
     constructor(address _nftContract) {
         nftContract = IERC1155(_nftContract);
@@ -30,18 +50,19 @@ struct ListedToken {
     address payable seller;
     address payable buyer;
     address payable owner;
-    bool currentlyListed;
-    bool sold;
-    bool isForSale;
-}
-
-struct Fee {
     uint256 buyerFee;
     uint256 sellerFee;
 }
 
+struct Check {
+    bool currentlyListed;
+    bool sold;
+    bool isForSale;
+    bool isCancelled;
+}
+
 mapping (uint256 => ListedToken) private listedToken;
-mapping (uint256 => Fee) private fees;
+mapping (uint256 => Check) private check;
 
 
 modifier onlyOwner() {
@@ -53,20 +74,22 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
     ListedToken storage _listedToken = listedToken[_tokenid];
     _listedToken.seller = payable(msg.sender);
     require(nftContract.balanceOf(msg.sender, _tokenid) != 0, "you do not own this NFT");
-    require(!_listedToken.sold, "This token Id has been sold"); 
+    require(!check[_tokenid].isForSale, "This token Id has been sold"); 
     // require(_listedToken.tokenId > 0, "Token does not exist");
     // require(_listedToken.royalty > 0 && _listedToken.royalty < 30, "royalty should be less than 30");
     // require(nftContract.ownerOf(_tokenid) == msg.sender, "You do not own an NFT");
 
     _listedToken.tokenId =  _tokenid;
-    _listedToken.isForSale = forSale;
-    _listedToken.sold = _sold;
+    check[_tokenid].isForSale = forSale;
+    check[_tokenid].sold = _sold;
     _listedToken.amount = _amount;
     _listedToken.price = _price;
     // _listedToken.royalty = _royalty;
-    _listedToken.currentlyListed = _listed;
+    check[_tokenid].currentlyListed = _listed;
 
     IERC1155(nftContract).safeTransferFrom(msg.sender, address(this), _tokenid, _amount, "");
+
+    emit ListedProperty(msg.sender, _tokenid, _price, _amount, block.timestamp, _listed, forSale, _sold);
     }
 
     function userNFTBal(address _addr, uint256 _tokenid) external view returns (uint256) {
@@ -90,9 +113,11 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
 
         uint256 price = listedToken[_tokenid].price;
         _listedToken.buyer = payable(msg.sender);
-        require(listedToken[_tokenid].isForSale, "Id is not for sale");
+        require(check[_tokenid].isForSale, "Id is not for sale");
         require(msg.value >= price, "Price is not complete");
-        require(listedToken[_tokenid].currentlyListed, "Id is not for currently listed");
+        require(check[_tokenid].currentlyListed, "Id is not for currently listed");
+        require(!check[_tokenid].sold, "This token id has been sold");
+        require(!check[_tokenid].isCancelled, "this sales has been canceled");
 
         uint256 buyerFee = calBuyerfee(listedToken[_tokenid].price);
         uint256 finalFee = price - buyerFee;
@@ -101,7 +126,13 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
         IERC1155(nftContract).safeTransferFrom(address(this), msg.sender, _tokenid, _amount, "");
     }
 
-    // function cancelSale(uint256 _tokenid) external 
+    function cancelSale(uint256 _tokenid, bool _isCancelled) external {
+        require(listedToken[_tokenid].seller == msg.sender, "you are not the owner");
+        require(check[_tokenid].isForSale, "this token is not listed for sale");
+        require(!check[_tokenid].sold, "this token Id has been sold");
+
+        check[_tokenid].isCancelled = _isCancelled;
+    }
 
     function getMarketplaceBalance() external view returns (uint256) {
         return address(this).balance;
