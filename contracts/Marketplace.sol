1c136018e44c1b3d2c8d13cbadbd730bc24049de
tehilafavourite <tehilafavourite@gmail.com>
2022-12-14 16:12:34 +0100

    buy and sell offer

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -6,106 +6,142 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
-contract  Marketplace is ERC1155Holder {
-
+contract Marketplace is ERC1155Holder {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     Counters.Counter private _nftSold;
     IERC1155 private nftContract;
     address private owner;
 
-event ListedProperty
-    (
-        address indexed _seller, 
-        uint256 _tokenId, 
+    event ListedProperty(
+        address indexed _seller,
+        uint256 _tokenId,
         uint256 _price,
-        uint256 _amount, 
-        uint256 _time, 
+        uint256 _amount,
+        uint256 _time,
         bool _forSale,
         bool _currentlyListed,
         bool sold
-        );
+    );
 
-event BuyEvent 
-    (
-    address _buyer,
-    uint256 _tokenId,
-    uint256 _amountOfToken,
-    uint256 _price, 
-    uint256 _time
-  );
+    event BuyEvent(
+        address _buyer,
+        uint256 _tokenId,
+        uint256 _amountOfToken,
+        uint256 _price,
+        uint256 _time
+    );
 
-event CancelSale 
-    (
-    address _seller,
-    uint256 _tokenId,
-    bool _isCanceled,
-    uint256 _time
-  );
-
+    event CancelSale(
+        address _seller,
+        uint256 _tokenId,
+        bool _isCanceled,
+        uint256 _time
+    );
 
     constructor(address _nftContract) {
         nftContract = IERC1155(_nftContract);
     }
 
-
-struct ListedToken {
-    uint256 tokenId;
-    // uint256 nftId;
-    uint256 amount;
-    uint256 price;
-    // uint256 royalty;
-    address payable seller;
-    address payable buyer;
-    address payable owner;
-    uint256 buyerFee;
-    uint256 sellerFee;
-}
-
-struct Check {
-    bool currentlyListed;
-    bool sold;
-    bool isForSale;
-    bool isCancelled;
-}
-
-mapping (uint256 => ListedToken) private listedToken;
-mapping (uint256 => Check) private check;
-
-
-modifier onlyOwner() {
-    require(owner == msg.sender, "Only owner can call this function");
-    _;
-}
-
-function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed, bool forSale, bool _sold) external {
-    ListedToken storage _listedToken = listedToken[_tokenid];
-    _listedToken.seller = payable(msg.sender);
-    require(nftContract.balanceOf(msg.sender, _tokenid) != 0, "you do not own this NFT");
-    require(!check[_tokenid].isForSale, "This token Id has been sold"); 
-    // require(_listedToken.tokenId > 0, "Token does not exist");
-    // require(_listedToken.royalty > 0 && _listedToken.royalty < 30, "royalty should be less than 30");
-    // require(nftContract.ownerOf(_tokenid) == msg.sender, "You do not own an NFT");
-
-    _listedToken.tokenId =  _tokenid;
-    check[_tokenid].isForSale = forSale;
-    check[_tokenid].sold = _sold;
-    _listedToken.amount = _amount;
-    _listedToken.price = _price;
-    // _listedToken.royalty = _royalty;
-    check[_tokenid].currentlyListed = _listed;
-
-    IERC1155(nftContract).safeTransferFrom(msg.sender, address(this), _tokenid, _amount, "");
-
-    emit ListedProperty(msg.sender, _tokenid, _price, _amount, block.timestamp, _listed, forSale, _sold);
+    struct ListedToken {
+        uint256 tokenId;
+        // uint256 nftId;
+        uint256 amount;
+        uint256 price;
+        // uint256 royalty;
+        address payable seller;
+        address payable buyer;
+        address payable owner;
+        uint256 buyerFee;
+        uint256 sellerFee;
     }
 
-    function userNFTBal(address _addr, uint256 _tokenid) external view returns (uint256) {
+    struct Check {
+        bool currentlyListed;
+        bool sold;
+        bool isForSale;
+        bool isCancelled;
+    }
+
+    struct SellOffer {
+        address seller;
+        uint256 minAmt;
+    }
+
+    struct BuyOffer {
+        address buyer;
+        uint256 price;
+        uint256 createTime;
+    }
+
+    mapping(uint256 => ListedToken) private listedToken;
+    mapping(uint256 => Check) private check;
+    mapping(uint256 => SellOffer) public activeSellOffers;
+    mapping(uint256 => BuyOffer) public activeBuyOffers;
+
+
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Only owner can call this function");
+        _;
+    }
+
+    function listNft(
+        uint256 _tokenid,
+        uint256 _amount,
+        uint256 _price,
+        bool _listed,
+        bool forSale,
+        bool _sold
+    ) external {
+        ListedToken storage _listedToken = listedToken[_tokenid];
+        _listedToken.seller = payable(msg.sender);
+        require(
+            nftContract.balanceOf(msg.sender, _tokenid) != 0,
+            "you do not own this NFT"
+        );
+        require(!check[_tokenid].isForSale, "This token Id has been sold");
+        // require(_listedToken.tokenId > 0, "Token does not exist");
+        // require(_listedToken.royalty > 0 && _listedToken.royalty < 30, "royalty should be less than 30");
+        // require(nftContract.ownerOf(_tokenid) == msg.sender, "You do not own an NFT");
+
+        _listedToken.tokenId = _tokenid;
+        check[_tokenid].isForSale = forSale;
+        check[_tokenid].sold = _sold;
+        _listedToken.amount = _amount;
+        _listedToken.price = _price;
+        // _listedToken.royalty = _royalty;
+        check[_tokenid].currentlyListed = _listed;
+
+        IERC1155(nftContract).safeTransferFrom(
+            msg.sender,
+            address(this),
+            _tokenid,
+            _amount,
+            ""
+        );
+
+        emit ListedProperty(
+            msg.sender,
+            _tokenid,
+            _price,
+            _amount,
+            block.timestamp,
+            _listed,
+            forSale,
+            _sold
+        );
+    }
+
+    function userNFTBal(
+        address _addr,
+        uint256 _tokenid
+    ) external view returns (uint256) {
         return nftContract.balanceOf(_addr, _tokenid);
     }
 
-    function calBuyerfee(uint256 fee) internal pure returns(uint256) {
-        return fee * 10 / 100;
+    function calBuyerfee(uint256 fee) internal pure returns (uint256) {
+        return (fee * 10) / 100;
     }
 
     // function calSellerfee(uint256 fee) internal pure returns(uint256) {
@@ -123,7 +159,10 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
         _listedToken.buyer = payable(msg.sender);
         require(check[_tokenid].isForSale, "Id is not for sale");
         require(msg.value >= price, "Price is not complete");
-        require(check[_tokenid].currentlyListed, "Id is not for currently listed");
+        require(
+            check[_tokenid].currentlyListed,
+            "Id is not for currently listed"
+        );
         require(!check[_tokenid].sold, "This token id has been sold");
         require(!check[_tokenid].isCancelled, "this sales has been canceled");
 
@@ -131,14 +170,22 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
         uint256 finalFee = price - buyerFee;
 
         payable(listedToken[_tokenid].seller).transfer(finalFee);
-        IERC1155(nftContract).safeTransferFrom(address(this), msg.sender, _tokenid, _amount, "");
+        IERC1155(nftContract).safeTransferFrom(
+            address(this),
+            msg.sender,
+            _tokenid,
+            _amount,
+            ""
+        );
 
         emit BuyEvent(msg.sender, _tokenid, _amount, finalFee, block.timestamp);
-
     }
 
     function cancelSale(uint256 _tokenid, bool _isCancelled) external {
-        require(listedToken[_tokenid].seller == msg.sender, "you are not the owner");
+        require(
+            listedToken[_tokenid].seller == msg.sender,
+            "you are not the owner"
+        );
         require(check[_tokenid].isForSale, "this token is not listed for sale");
         require(!check[_tokenid].sold, "this token Id has been sold");
 
@@ -154,8 +201,4 @@ function listNft(uint256 _tokenid, uint256 _amount, uint256 _price, bool _listed
     function getUserBalance(address _user) external view returns (uint256) {
         return address(_user).balance;
     }
-
-
 }
-
-
