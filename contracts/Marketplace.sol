bf1b1e1e6f80fe4167bef33546373546845bd041
tehilafavourite <tehilafavourite@gmail.com>
2022-12-20 00:31:50 +0100

    IERC2981 was added

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "./IERC2981.sol";
+
 
 contract  Marketplace is ERC1155Holder {
 
